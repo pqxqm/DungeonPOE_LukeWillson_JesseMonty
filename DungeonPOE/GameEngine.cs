4b967c774d8b03d1bdc882cb9ca0e365f32a3946
Luke Willson <luke.willson46@gmail.com>
2026-07-23 14:10:00 +0200

    Q2.5 Done

---
+++ DungeonPOE/GameEngine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DungeonPOE
+{
+    internal class GameEngine
+    {
+        private Level currentLevel;
+        private int numberOfLevels;
+        private Random random;
+
+        private const int MIN_SIZE = 10;
+        private const int MAX_SIZE = 20;
+
+        public GameEngine(int newnumberOfLevels)
+        {
+            numberOfLevels = newnumberOfLevels;
+            random = new Random();
+
+            int width = random.Next(MIN_SIZE);
+            int height = random.Next(MAX_SIZE); // will only go up to 19 btw, cause of how it works
+
+            currentLevel = new Level(width, height);
+        }
+
+        public string ToString()
+        {
+            return currentLevel.ToString();
+        }
+    }
+}
