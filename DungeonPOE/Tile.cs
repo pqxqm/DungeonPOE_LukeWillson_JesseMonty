39b2f6216e9f5647ebe970d00718ba98a4afb158
Luke Willson <luke.willson46@gmail.com>
2026-07-23 12:52:07 +0200

    Q2.2 DONE

---
+++ DungeonPOE/Tile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Policy;
+using System.Text;
+
+namespace DungeonPOE
+{
+    internal abstract class Tile
+    {
+        private Position position;
+
+        public Tile(Position position)
+        {
+            this.position = position;
+        }
+
+        public int X
+        {
+            get { return position.X; }
+        }
+        public int Y
+        {
+            get { return position.Y; }
+        }
+        public abstract char Display
+        {
+            get;
+        }
+    }
+}
