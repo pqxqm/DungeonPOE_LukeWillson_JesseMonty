9b5e295ac0069f351d4de2b9a45fd0ee08ee6c74
Luke Willson <luke.willson46@gmail.com>
2026-07-23 12:42:35 +0200

    Q2.1 done

---
+++ DungeonPOE/position.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DungeonPOE
+{
+    internal class Position //allows us to store the XY - coordinates of a single tile
+    {
+        private int x;
+        private int y;
+
+        public Position(int newx, int newy)
+        {
+            x = newx;
+            y = newy;
+        }
+
+        public int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+    }
+}
